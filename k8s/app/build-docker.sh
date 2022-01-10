5572e274fca2c66119d9ee3693b7c89a5758a243
jialiang.tjl <jialiang.tjl@alibaba-inc.com>
2022-01-11 00:13:24 +0800

    <chore>:(k8s):More

--- k8s/app/build-docker.sh
+++ k8s/app/build-docker.sh
@@ -1,3 +1,10 @@
 docker build . -t jialtang/greeter:v1.0.0
 
 kind load docker-image jialtang/greeter:v1.0.0 --name my-cluster
+
+
+# Reload k8s.
+
+kubectl delete -f ../deployment/greeter.yaml
+
+kubectl apply -f ../deployment/greeter.yaml
